1719031c4b82496dddfe2ebe5f866d55a3cabf6d
makarenkoelena <g00341964@gmit.ie>
2019-02-19 15:05:03 +0000

    returned markers' positions

--- GoingMyWay/src/app/journey-planner/journey-planner.page.ts
+++ GoingMyWay/src/app/journey-planner/journey-planner.page.ts
@@ -1,5 +1,15 @@
+import {
+  GoogleMaps,
+  GoogleMap,
+  GoogleMapsEvent,
+  GoogleMapOptions,
+  CameraPosition,
+  MarkerOptions,
+  Marker,
+  Environment,
+  ILatLng
+} from '@ionic-native/google-maps';
 import { Component, OnInit } from '@angular/core';
-import { GoogleMaps, GoogleMap, Marker, LatLng, ILatLng } from '@ionic-native/google-maps/ngx';
 
 @Component({
   selector: 'app-journey-planner',
@@ -7,18 +17,68 @@ import { GoogleMaps, GoogleMap, Marker, LatLng, ILatLng } from '@ionic-native/go
   styleUrls: ['./journey-planner.page.scss'],
 })
 export class JourneyPlannerPage implements OnInit {
-  map:GoogleMap;
+  map: GoogleMap;
+  source: ILatLng;
+  target: ILatLng;
+  posA: ILatLng =
+    {
+      lat: 53.270962,
+      lng: -9.062691
+    };
+  posB: ILatLng =
+    {
+      lat: 54.270962,
+      lng: -9.062691
+    };
   constructor() { }
 
   ngOnInit() {
-    this.map = GoogleMaps.create('myMap');
-  }
-  planJourney(){
-    console.log("clicked!");
-    //when this function is used
-    //console.log co-ordinates of marker and marker b
-    //also a name-- 
-    //these are for database
+    this.loadMap();
   }
 
+  loadMap() {
+    // This code is necessary for browser
+    // Environment.setEnv({
+    //   'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyDQUaBvR0ZXMXsn-sPP6qhPw-qGFYmsTMs',
+    //   'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyDQUaBvR0ZXMXsn-sPP6qhPw-qGFYmsTMs'
+    // });
+    let mapOptions: GoogleMapOptions = {
+      camera: {
+        target: {
+          lat: 53.270962,
+          lng: -9.062691
+        },
+        zoom: 5,
+        tilt: 30
+      }
+    };
+
+    this.map = GoogleMaps.create('myMap', mapOptions);
+
+
+    let marker: Marker = this.map.addMarkerSync({
+      title: "A",
+      icon: 'Blue',
+      position: this.posA,
+      draggable: true,
+    });
+    let marker2: Marker = this.map.addMarkerSync({
+      title: "B",
+      icon: 'Red',
+      position: this.posB,
+      draggable: true,
+    });
+
+
+    marker.on(GoogleMapsEvent.MARKER_CLICK).subscribe(() => {
+      this.source = marker.getPosition();
+      this.target = marker.getPosition();
+      this.showPosition(this.source.lat, this.source.lng, this.target.lat, this.target.lng)
+    });
+  }//loadMap()
+
+  showPosition(x1: number, y1: number, x2: number, y2: number) {
+    console.log("marker A: " + x1 + " " + y1 + " marker B " + x2 + " " + y2);
+
+  }
 }
