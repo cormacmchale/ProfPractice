56e6eea38bd503e096f5fbd2e6db0d5998af0b98
unknown <cormacmchale1@gmail.com>
2019-04-25 14:48:17 +0100

    journey planner code clean up

--- GoingMyWay/src/app/journey-planner/journey-planner.page.ts
+++ GoingMyWay/src/app/journey-planner/journey-planner.page.ts
@@ -11,10 +11,8 @@ import {
 } from '@ionic-native/google-maps';
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-import { Geocoder, GeocoderResult, BaseArrayClass } from '@ionic-native/google-maps/ngx';
+import { Geocoder, GeocoderResult } from '@ionic-native/google-maps/ngx';
 import { JourneyService } from 'src/app/journey.service';
-//hopefully will be used to fix async geodoing error
-import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-journey-planner',
@@ -23,18 +21,22 @@ import { AlertController } from '@ionic/angular';
 })
 export class JourneyPlannerPage implements OnInit {
 
+  //imports for planning a Journey and sending it to the database
+  //the map will be displayed for the user who can drag markers to start and end point
   map: GoogleMap;
   startJourney: Marker;
   endJourney: Marker;
+  //these will be the values that will be sent to that database as a document
   start: ILatLng;
   end: ILatLng;
 
-  constructor(private router: Router, private journeyService: JourneyService, private alertController: AlertController) { }
+  constructor(private router: Router, private journeyService: JourneyService) { }
 
   ngOnInit() {
     this.loadMap();
   }
 
+  //loads the map above ireland - will be changes in future builds for improved functionality
   loadMap() {
     let mapOptions: GoogleMapOptions = {
       camera: {
@@ -46,28 +48,25 @@ export class JourneyPlannerPage implements OnInit {
         tilt: 30
       }
     };
-
     this.map = GoogleMaps.create('myMap', mapOptions);
-  }//loadMap()
+  }
 
-  visitMapPage() {
-    this.router.navigate(['map']);
-  }
-  navigateJourneyPlanner() {
-    this.router.navigate(['journey-planner']);
-  }
+
   search(location: string) {
+    //clear the markers from the Map if the user decides to do a second search
+    //so as not to confuse the data that will be sent to the database
     this.map.clear();
-    console.log(location)
+
+    //the location typed in by the User is converted to latlng co-ordinates by the Geocoder Object
+    //the map is then sent to focus on this location
+    //the user is then given two markers to drag to a start location and end location
     Geocoder.geocode
       ({
         "address": location
       })
       .then((results: GeocoderResult[]) => {
-        console.log(results[0].position);
         this.map.setCameraTarget(results[0].position);
         this.map.setCameraZoom(10);
-        let mark: number = results[0].position.lat;
         this.endJourney = this.map.addMarkerSync({
           title: "End Journey",
           icon: 'Red',
@@ -83,26 +82,27 @@ export class JourneyPlannerPage implements OnInit {
         });
       })
   }
+  //this is the beginning of the largest set of function calls in the app
+  //its setup in a cascading way as to handle the asynchrounous nature of the Geocoding
   addJourneyToDatabase() {
+    //first check that someone is logged in
+    //if not found then alert the User as a user is required for a Journey
     if (!this.journeyService.getUser()) {
       alert("Please Log in to add a Journey")
     }
     else {
+      //first gather the required information from the user that is logged on from the device
       let user: any = this.journeyService.getUser();
       let userName: string = user.email
+      //get the positions of where they have dragged the start and end of journey markers
       this.start = this.startJourney.getPosition();
       this.end = this.endJourney.getPosition();
+      //all this information is passed here for formatting to be sent to that database as a document
       this.getJourneyInformation(this.start.lat, this.start.lng, this.end.lat, this.end.lng, userName)
-      //this.showPosition(this.start.lng, this.start.lat, this.end.lng, this.end.lat, userName)
     }
   }
-
-  addToDataBase(x1: number, y1: number, x2: number, y2: number, user: string, startLoc: string, endLoc: string) {
-    this.journeyService.sendJourney(x1, y1, x2, y2, user, startLoc, endLoc);
-    alert("Journey added!")
-    this.router.navigate(['home']);
-  }
-
+  //this function reverse Geocodes a latlng for the start address
+  //this information is used to delete a Journey in the databasePage
   getJourneyInformation(x: number, y: number, x2: number, y2: number, user: string) {
     let waitForAddress: string = null
     let checkAddress: string[]
@@ -113,15 +113,17 @@ export class JourneyPlannerPage implements OnInit {
             "lat": x,
             "lng": y
           }
+        //this is asynchronous so all of the information must be handled here for consistancy
       }).then((results: GeocoderResult[]) => {
-        console.log(results)
         checkAddress = results[0].extra.lines
         waitForAddress =
           checkAddress[0] + ", " + checkAddress[1] + ", " + checkAddress[2]
           + ", " + checkAddress[3] + ", " + checkAddress[4]
+        //the same logic is then used to geocode the end address
         this.finishGetJourneyInformation(x, y, x2, y2, user, waitForAddress)
       })
   }
+  //the same as the above function for the other street addess of the end loaction
   finishGetJourneyInformation(x: number, y: number, x2: number, y2: number, user: string, startLoc: string) {
     let waitForAddress: string = null
     let checkAddress: string[];
@@ -133,7 +135,6 @@ export class JourneyPlannerPage implements OnInit {
             "lng": y2
           }
       }).then((results: GeocoderResult[]) => {
-        console.log(results)
         checkAddress = results[0].extra.lines
         waitForAddress =
           checkAddress[0] + ", " + checkAddress[1] + ", " + checkAddress[2]
@@ -141,5 +142,12 @@ export class JourneyPlannerPage implements OnInit {
         this.addToDataBase(y, x, y2, x2, user, startLoc, waitForAddress)
       })
   }
-
+  //finally after the street addresses have been obtained from the co-ordinates of where the markers are then
+  //send all of the information to the service to be stored on firebase as a document
+  addToDataBase(x1: number, y1: number, x2: number, y2: number, user: string, startLoc: string, endLoc: string) {
+    this.journeyService.sendJourney(x1, y1, x2, y2, user, startLoc, endLoc);
+    //alert the user and return them to the homepage
+    alert("Journey added!")
+    this.router.navigate(['home']);
+  }
 }
